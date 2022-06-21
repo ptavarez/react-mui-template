9aacb2f258fa246c6660b0c097a2195f1f94f96f
ptavarez <ptavarez16@gmail.com>
2022-06-21 15:19:39 -0400

    Add MUI and Axios Dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import ProTip from './components/ProTip';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Copyright() {
+	return (
+		<Typography variant='body2' color='text.secondary' align='center'>
+			{'Copyright © '}
+			<Link color='inherit' href='https://mui.com/'>
+				Your Website
+			</Link>{' '}
+			{new Date().getFullYear()}.
+		</Typography>
+	);
 }
 
-export default App;
+export default function App() {
+	return (
+		<Container maxWidth='sm'>
+			<Box sx={{ my: 4 }}>
+				<Typography variant='h5' component='h1' gutterBottom>
+					Create React App with TypeScript and MUI
+				</Typography>
+				<ProTip />
+				<Copyright />
+			</Box>
+		</Container>
+	);
+}
